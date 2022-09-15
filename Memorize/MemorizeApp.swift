f72828b69e22bb429525d698ef711f5472c323c0
humbsj <humberto.sjar@gmail.com>
2022-09-15 16:06:04 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Humberto Salcedo Jara on 15/09/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
